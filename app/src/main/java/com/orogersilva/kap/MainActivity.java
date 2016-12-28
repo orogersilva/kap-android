fc5111d300b353501c85fca5d4996c264d99988a
Roger Silva <orogersilva@gmail.com>
2016-12-28 16:17:20 -0200

    Prepare version 0.1.0.

---
+++ app/src/main/java/com/orogersilva/kap/MainActivity.java
@@ -0,0 +1,18 @@
+package com.orogersilva.kap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // region ACTIVITY LIFECYCLE METHODS
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    // endregion
+}
